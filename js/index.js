11d6acd0bdb145a5e5934a815a5a6ce16c6f0e0a
SimoTheGamer <74712999+SimoTheGamer@users.noreply.github.com>
2022-05-28 23:02:54 +0200

    .

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ var testoyt = document.getElementById("yt");
 var linkyt = "link" + value.testoyt;
 
 console.log(value.testoyt)
+
+
+window.onload = function () {
+    OpenBootstrapPopup();
+};
+function OpenBootstrapPopup() {
+    $("#myModal").modal('show');
+}
