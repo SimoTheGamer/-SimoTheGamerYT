02f51a44a27653cfa006188e78cda375845ac637
SimoTheGamer <74712999+SimoTheGamer@users.noreply.github.com>
2023-11-12 23:50:18 +0100

    .

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+//RICERCA YOUTUBE
+function ytr(nome) {
+    var nome = document.getElementById("nome").value;
+    var link = "https://www.youtube.com/channel/UCo_DLowjBpvmiyYpsfH7KYQ/search?query=" + nome;
+    window.open(link);
+}
--- js/index.js
+++
@@ -1,9 +0,0 @@
-//RICERCA YOUTUBE
-function ytr(nome) {
-    var nome = document.getElementById("nome").value;
-    var link = "https://www.youtube.com/channel/UCo_DLowjBpvmiyYpsfH7KYQ/search?query=" + nome;
-    window.open(link);
-}
-
-
-
