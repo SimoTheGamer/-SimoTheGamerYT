769853f5e42768d4e1e4f014fd7fe1a5b6571a3b
SimoTheGamer <74712999+SimoTheGamer@users.noreply.github.com>
2023-11-13 19:29:37 +0100

    .

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,8 @@ function ytr(nome) {
     var link = "https://www.youtube.com/channel/UCo_DLowjBpvmiyYpsfH7KYQ/search?query=" + nome;
     window.open(link);
 }
+
+let logoController = gsap.timeline({ repeat: -1, yoyo: true, ease: Power2.easeIn });
+
+
+logoController.fromTo('#logo-controller', { y: 10, duration: 2 }, { y: -20, duration: 1 })
