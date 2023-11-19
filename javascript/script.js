8ce79776d819ed228ccf59002c6c402258b6b565
SimoTheGamer <74712999+SimoTheGamer@users.noreply.github.com>
2023-11-19 20:36:09 +0100

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,14 @@
 //RICERCA YOUTUBE
 function ytr(nome) {
-    var nome = document.getElementById("nome").value;
     var link = "https://www.youtube.com/channel/UCo_DLowjBpvmiyYpsfH7KYQ/search?query=" + nome;
     window.open(link);
 }
 
-let logoController = gsap.timeline({ repeat: -1, yoyo: true, ease: Power2.easeIn });
 
 
-logoController.fromTo('#logo-controller', { y: 10, duration: 2 }, { y: -20, duration: 1 })
+//let logoController = gsap.timeline({ repeat: -1, yoyo: true, ease: Power2.easeIn });
+
+
+//ogoController.fromTo('#logo-controller', { y: 10, duration: 2 }, { y: -20, duration: 1 })
+
+//ytr("aternos")
